57c0cd188f3063228f2c058f8261296bf2ad4510
vojtecholcman <vojtech.holcman.s@gyarab.cz>
2026-01-09 12:14:47 +0100

    python turtle prvni.py

---
+++ zelva/prvni.py
@@ -0,0 +1,21 @@
+from turtle import forward, left, exitonclick, right
+from math import sqrt
+from random import randint
+
+def domecek(a):
+    c = sqrt(2*a**2)
+    forward(a)
+    left(90)
+    forward(a)
+    left(90)
+    forward(a) 
+    left(90)
+    forward(a)
+    left(90)
+    forward(a)
+
+for i in range(10):
+    domecek(randint(10, 20))
+    right(36)
+
+exitonclick()
