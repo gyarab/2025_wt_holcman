a35a9f84a6f553fe15c8177a57f05688e7497d09
vojtecholcman <vojtech.holcman.s@gyarab.cz>
2026-01-17 10:05:11 +0100

    edited exchange.py and requi.txt

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,4 +1,7 @@
 import httpx
 
-r = httpx.get("https://gyarab.github.io/2025_wt_holcman/")
+r = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
 print(r.text)
+print(r.status)
+lines = r.text.split('\n')
+print(lines[0])
