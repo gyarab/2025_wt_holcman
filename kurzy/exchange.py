7194249720f629fb6348d34e0fef6e3a7e863298
vojtecholcman <vojtech.holcman.s@gyarab.cz>
2026-01-23 12:11:14 +0100

    edited exchange.py

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,7 +1,23 @@
 import httpx
 
-r = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
-print(r.text)
-print(r.status)
-lines = r.text.split('\n')
-print(lines[0])
+res = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
+
+print("server odpovedel:", res.status_code)
+lines = res.text.split('\n')
+print("Kruzy pro den:", lines[0].split(" ")[0])
+
+line_euro = ""
+for line in lines:
+    if "EUR" in line:
+        line_euro = line
+        break
+
+rate_str = line_euro.split('|')[-1].replace(',' , '.')
+rate = float(rate_str)
+
+print("Kurz eura je", rate)
+
+value_in = float(input("Kolik mas eur?"))
+value_out = value_in * rate
+
+print(f"Tak to je {value_out:.2f} korun.")
